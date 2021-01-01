7a74fa042b832e7608c4e6f2a0d67a8338646cd2
Jonathan Rosenboim <masterfqf@gmail.com>
2021-01-01 18:50:55 +0200

    Initialize RunContainer function

--- vendor/runtime/run.go
+++ vendor/runtime/run.go
@@ -1,5 +1,20 @@
 package runtime
 
-func RunContainer(containersDir string, name string) {
-	
+import (
+	"log"
+	"oci"
+	"strings"
+)
+
+func RunContainer(imagesDir string, containersDir string, name string, image string) {
+	imageName, imageTag := parseImageName(image)
+	oci.UnpackImage(imagesDir, containersDir, name, imageName, imageTag)
+}
+
+func parseImageName(name string) (string, string) {
+	split := strings.Split(name, ":")
+	if len(split) != 2 {
+		log.Fatal("Invalid image name")
+	}
+	return split[0], split[1]
 }
