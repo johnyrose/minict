ccbdb1c304855a989e96b2cf38d0d58fc5031bc0
Jonathan Rosenboim <masterfqf@gmail.com>
2021-01-02 14:40:51 +0200

    Implement removing of container

---
+++ vendor/run/remove.go
@@ -0,0 +1,28 @@
+package run
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/apex/log"
+)
+
+func RemoveContainer(imagesDir string, containersDir string, name string) {
+	containerDir := fmt.Sprintf("%s/%s", containersDir, name)
+	imageConfig := GetImageConfig(containerDir)
+	unmountAll(containerDir, imageConfig)
+	err := os.RemoveAll(containerDir)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to delete container files. Error received: %s", err.Error()))
+	}
+}
+
+func unmountAll(containerDir string, config ImageConfig) {
+	for _, mount := range config.MountsConfig {
+		err := syscall.Unmount(fmt.Sprintf("%s/rootfs/%s", containerDir, mount.Destination), 0)
+		if err != nil {
+			log.Error(fmt.Sprintf("Failed to umount %s, error received: %s", mount.Destination, err.Error()))
+		}
+	}
+}
