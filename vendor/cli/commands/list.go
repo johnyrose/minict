b8d4477f9981b58c51d99a4070c9b4fccb8b1cf4
Jonathan Rosenboim <masterfqf@gmail.com>
2021-01-02 12:34:25 +0200

    Implement listContainers function

---
+++ vendor/cli/commands/list.go
@@ -0,0 +1,20 @@
+package commands
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+func listContainers(containersDir string) []string {
+	files, err := ioutil.ReadDir(containersDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var containers []string
+	for _, file := range files {
+		if file.IsDir() {
+			containers = append(containers, file.Name())
+		}
+	}
+	return containers
+}
